371d6ddc2e553de7b908d528bcdd5aad0f4cc9f7
VictoriaKwamboka <victoriamonari9815@gmail.com>
2022-03-06 14:33:13 +0300

    Define arrays with female and male Akan names

--- script/script.js
+++ script/script.js
@@ -7,6 +7,21 @@
 $('.datepicker').pickadate()
 let yearOfBirth = new Date(date)
 const birthday = yearOfBirth.getDay();
-alert(birthday)
 
 
+
+
+
+//define arrays with days of the week
+let daysOfWeek = [
+    "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+];
+
+// create arrays with female and male Akan names
+let maleNames = [
+    "Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"
+];
+
+let femaleNames = [
+    "Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"
+]
